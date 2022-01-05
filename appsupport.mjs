629f0d4c53a3b265e9c54212f2fc13c6b83c9008
TunrayoIlawole <tunrayoilawole99@gmail.com>
2022-01-05 13:46:16 +0100

    initial commit

---
+++ appsupport.mjs
@@ -0,0 +1,75 @@
+import { port, server } from './app.mjs';
+import { default as DBG } from 'debug';
+const debug = DBG('notes:debug'); 
+const dbgerror = DBG('notes:error'); 
+
+import * as util from 'util';
+
+process.on('uncaughtException', function(err) {
+    console.error(`I've crashed!!! - ${(err.stack || err)}`);
+});
+
+process.on('unhandledRejection', (reason, p) => {
+    console.error(`Unhandled Rejection at ${util.inspect(p) } reason: ${reason}`);
+});
+
+export function normalizePort(val) {
+    const port = parseInt(val, 10);
+    if (isNaN(port)) {
+        return val;
+    }
+    if (port >= 0) {
+        return port;
+    }
+
+    return false;
+}
+
+export function onError(error) {
+    dbgerror(error);
+
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+
+    const bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
+
+    switch (error.code) {
+        case 'EACCESS':
+            console.error(`${bind} requires elevated priviledges`);
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(`${bind} is already in use`);
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+}
+
+export function onListening() {
+    const addr = server.address();
+    const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
+
+    debug(`Listening on ${bind}`);
+}
+
+export function handle404(req, res, next) {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+}
+
+export function basicErrorHandler(err, req, res, next) {
+    if (res.headersSent) {
+        return next(err);
+    }
+
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+}
---
+++ routes/index.mjs
@@ -0,0 +1,20 @@
+import * as express  from 'express';
+import { NotesStore as notes } from '../app.mjs';
+export const router = express.Router();
+
+/* GET home page. */
+router.get('/', async function(req, res, next) {
+  try {
+    const keylist = await notes.keylist();
+
+    const keyPromises = keylist.map(key => {
+      return notes.read(key);
+    });
+    
+    const notelist = await Promise.all(keyPromises);
+
+    res.render('index', { title: 'Notes', notelist: notelist });
+  } catch (err) {
+    next(err);
+  }
+});
