9ef00e54ef5e14099a14fe4b97a54c94f62381c1
TunrayoIlawole <tunrayoilawole99@gmail.com>
2022-01-09 14:01:09 +0100

    implement sqlite database

--- models/notes-store.mjs
+++ models/notes-store.mjs
@@ -1,3 +1,4 @@
+
 import { default as DBG } from 'debug';
 const debug = DBG('notes:notes-store');
 const error = DBG('notes:error-store');
@@ -7,11 +8,13 @@ var _NotesStore;
 export async function useModel(model) {
     try {
         let NotesStoreModule = await import(`./notes-${model}.mjs`);
+        debug(NotesStoreModule);
         let NotesStoreClass = NotesStoreModule.default;
+        debug(NotesStoreClass);
         _NotesStore = new NotesStoreClass();
         return _NotesStore;
     } catch (err) {
-        throw new Error (`No recognized NotesStore in ${model} because ${err}`);
+        throw new Error(`No recognized NotesStore in ${model} because ${err}`);
     }
 }
 
